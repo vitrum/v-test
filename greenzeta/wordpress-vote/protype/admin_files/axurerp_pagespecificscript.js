4eb3ec6452a1cb5893831cdee09aeb954310a2e6
vitrum.zhu <vitrum.zhu@hotmail.com>
2012-04-13 20:56:21 +0800

    vote

---
+++ greenzeta/wordpress-vote/protype/admin_files/axurerp_pagespecificscript.js
@@ -0,0 +1,122 @@
+﻿
+var PageName = 'admin';
+var PageId = '1afd560145654003a8fb302e9d34cafa'
+var PageUrl = 'admin.html'
+document.title = 'admin';
+var PageNotes = 
+{
+"pageName":"admin",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '13');
+  value = value.replace(/\[\[GenMonth\]\]/g, '4');
+  value = value.replace(/\[\[GenMonthName\]\]/g, '四月');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, '星期五');
+  value = value.replace(/\[\[GenYear\]\]/g, '2012');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u31 = document.getElementById('u31');
+
+var u16 = document.getElementById('u16');
+gv_vAlignTable['u16'] = 'top';
+var u17 = document.getElementById('u17');
+
+var u28 = document.getElementById('u28');
+gv_vAlignTable['u28'] = 'center';
+var u29 = document.getElementById('u29');
+
+var u8 = document.getElementById('u8');
+gv_vAlignTable['u8'] = 'center';
+var u30 = document.getElementById('u30');
+gv_vAlignTable['u30'] = 'center';
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'center';
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'center';
+var u32 = document.getElementById('u32');
+gv_vAlignTable['u32'] = 'center';
+var u13 = document.getElementById('u13');
+
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'center';
+var u15 = document.getElementById('u15');
+gv_vAlignTable['u15'] = 'top';
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u26 = document.getElementById('u26');
+gv_vAlignTable['u26'] = 'center';
+var u10 = document.getElementById('u10');
+gv_vAlignTable['u10'] = 'center';
+var u11 = document.getElementById('u11');
+gv_vAlignTable['u11'] = 'top';
+var u3 = document.getElementById('u3');
+
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'top';
+var u9 = document.getElementById('u9');
+
+var u27 = document.getElementById('u27');
+
+var u7 = document.getElementById('u7');
+
+var u23 = document.getElementById('u23');
+
+var u24 = document.getElementById('u24');
+gv_vAlignTable['u24'] = 'center';
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'center';
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'top';
+var u18 = document.getElementById('u18');
+gv_vAlignTable['u18'] = 'center';
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'top';
+var u20 = document.getElementById('u20');
+
+var u5 = document.getElementById('u5');
+
+var u22 = document.getElementById('u22');
+
+var u25 = document.getElementById('u25');
+
+var u33 = document.getElementById('u33');
+
+var u34 = document.getElementById('u34');
+
+var u0 = document.getElementById('u0');
+
+if (window.OnLoad) OnLoad();
---
+++ greenzeta/wordpress-vote/protype/data/sitemap.js
@@ -0,0 +1,21 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Greenzeta Vote",
+"type":"Wireframe",
+"url":"Greenzeta_Vote.html",
+"children":[
+{
+"pageName":"blog",
+"type":"Wireframe",
+"url":"blog.html",
+"children":[
+{
+"pageName":"finish vote",
+"type":"Wireframe",
+"url":"finish_vote.html"}]},
+{
+"pageName":"admin",
+"type":"Wireframe",
+"url":"admin.html"}]}]};
